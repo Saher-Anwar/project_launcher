fbcca4cf9527c9d1ab541a5fc471fb9dd9082095
Saher <saher.ziauddin@dal.ca>
2025-08-19 03:31:01 -0300

    Added basic read/write to mongodb

--- crud-service/src/main.js
+++ crud-service/src/main.js
@@ -4,16 +4,39 @@ const http = require('http');
 const PORT = 5000;
 const uri = 'mongodb+srv://dark_papa:dark_papa1234@mycluster1.iv9rj.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster1';
 const client = new MongoClient(uri);
+const databaseName = "development_db_for_project_launcher"
+const collectionName = "project_data";
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  write_to_db()
-  res.end('Added a document to MongoDB: ' + new Date().toISOString());
+  readAllDocuments()
 });
 
 server.listen(PORT, () => {
   // Only called once -> when server is ready to accept requests
 });
+
+async function readAllDocuments() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+
+    // Select database and collection
+    const database = client.db(databaseName);
+    const collection = database.collection(collectionName);
+
+    // Find all documents
+    const cursor = collection.find({}); // empty filter = all documents
+    const results = await cursor.toArray(); // convert cursor to array
+
+    console.log("All documents:");
+    console.log(results);
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    await client.close();
+  }
+}
 
 async function write_to_db() {
   try {
@@ -21,8 +44,8 @@ async function write_to_db() {
     await client.connect();
 
     // Select database and collection
-    const database = client.db("myDatabase");
-    const collection = database.collection("myCollection");
+    const database = client.db(databaseName);
+    const collection = database.collection(collectionName);
 
     // Document to insert
     const doc = {
